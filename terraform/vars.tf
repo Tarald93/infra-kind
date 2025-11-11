7e4133ec7ec340da0da1c72c0f595d3d33b141b9
Tarald <tsp09k@gmail.com>
2025-11-11 14:35:30 +0100

    updated vars

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -18,33 +18,33 @@ variable "kind_api_version" {
   type = string
   default = "kind.x-k8s.io/v1alpha4"
 }
-variable "github_org" {
-  description = "github_org"
-  type = string
-}
-variable "github_token" {
-  description = "github_token"
-  type = string
-}
-variable "github_repo_name" {
-  description = "github_repo"
-  type = string
-}
+# variable "github_org" {
+#   description = "github_org"
+#   type = string
+# }
+# variable "github_token" {
+#   description = "github_token"
+#   type = string
+# }
+# variable "github_repo_name" {
+#   description = "github_repo"
+#   type = string
+# }
 
-# Flux bootstrap settings
-variable "git_branch" { 
-  type = string
-  default = "main" 
-}
-variable "flux_namespace" {
-  type = string
-  default = "flux-system" 
-  }
-variable "flux_path" {
-  type = string
-  default = "../clusters/local"
-  }
-variable "git_known_hosts" {
-  type = string
-  default = ""
-  }
+# # Flux bootstrap settings
+# variable "git_branch" { 
+#   type = string
+#   default = "main" 
+# }
+# variable "flux_namespace" {
+#   type = string
+#   default = "flux-system" 
+#   }
+# variable "flux_path" {
+#   type = string
+#   default = "../clusters/local"
+#   }
+# variable "git_known_hosts" {
+#   type = string
+#   default = ""
+#   }
